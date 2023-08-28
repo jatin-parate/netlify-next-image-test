6e80f2d1cb2ef0af4d5fea6663f1d001c9fde232
Jatin <jatin4228@gmail.com>
2023-08-28 23:11:02 +0530

    added external image

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        hostname: "cdn.shopify.com",
+        port: "",
+        pathname: "/app-store/listing_images/**",
+        protocol: "https",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
